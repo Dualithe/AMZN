7e1bd49b1ba1f26ab4a6deabde2941c0f9c21c9b
Dualithe <dualithe@gmail.com>
2022-08-13 18:56:46 +0200

    Add pressure plate

---
+++ Assets/Scripts/pressurePlateHandler.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pressurePlateHandler : MonoBehaviour
+{
+    [SerializeField] float spawnBoxCooldown;
+    [SerializeField] float spawnBoxRange;
+    [SerializeField] Animator animator;
+    [SerializeField] GameObject box;
+    private float cd;
+    private bool colliding = false;
+
+    private void Start()
+    {
+        cd = spawnBoxCooldown;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (cd == 0 && (collision.transform.tag == "Player" || collision.transform.tag == "Robot"))
+        {
+            spawnBox();
+        }
+        colliding = true;
+        //animator.SetBool("isColliding", true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        cd = spawnBoxCooldown;
+        colliding = false;
+        //animator.SetBool("isColliding", false);
+    }
+
+    private void Update()
+    {
+        if (colliding == false)
+        {
+            cd -= Time.deltaTime;
+            cd = Mathf.Max(cd, 0);
+        }
+    }
+
+    private void spawnBox()
+    {
+        var spawnAngle = Random.Range(0, 2 * Mathf.PI);
+        var spawnDistance = Random.Range(0, spawnBoxRange);
+        Vector2 thisPos = transform.position;
+        Vector2 spawnVec = new Vector2(Mathf.Cos(spawnAngle), Mathf.Sin(spawnAngle));
+        Instantiate(box, ((spawnVec * spawnDistance) + thisPos), Quaternion.identity);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, spawnBoxRange);
+    }
+}
